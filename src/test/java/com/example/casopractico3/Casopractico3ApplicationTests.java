2d2c7366f31b95842586bee22367e457e6d27a46
maria <maridilo@myuax.com>
2025-11-26 18:46:51 +0100

    asdfghj

---
+++ src/test/java/com/example/casopractico3/Casopractico3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casopractico3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casopractico3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
